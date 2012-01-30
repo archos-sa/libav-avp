71e5714de638bdc6b4d198e02dca718d07637d26
Thomas Guillem <guillem@archos.com>
2012-01-30 12:08:34 +0100

    s,/tmp/lala,/dev/null,g

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ $(shell \
 	make -C $(call my-dir) -f configure.mk \
 	ANDROID_SRC=$(shell pwd) \
 	TARGET_PRODUCT=$(TARGET_PRODUCT) \
-	> /tmp/lala 2>&1)
+	> /dev/null 2>&1)
 
 include $(all-subdir-makefiles)
