d4334260070c21caafad6da38c415d2dd8fa572e
Diego Biurrun <diego@biurrun.de>
2012-10-01 12:48:23 +0200

    avformat: const correctness for av_hex_dump / av_hex_dump_log

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -1546,7 +1546,7 @@ enum AVCodecID av_guess_codec(AVOutputFormat *fmt, const char *short_name,
  *
  * @see av_hex_dump_log, av_pkt_dump2, av_pkt_dump_log2
  */
-void av_hex_dump(FILE *f, uint8_t *buf, int size);
+void av_hex_dump(FILE *f, const uint8_t *buf, int size);
 
 /**
  * Send a nice hexadecimal dump of a buffer to the log.
@@ -1560,7 +1560,7 @@ void av_hex_dump(FILE *f, uint8_t *buf, int size);
  *
  * @see av_hex_dump, av_pkt_dump2, av_pkt_dump_log2
  */
-void av_hex_dump_log(void *avcl, int level, uint8_t *buf, int size);
+void av_hex_dump_log(void *avcl, int level, const uint8_t *buf, int size);
 
 /**
  * Send a nice dump of a packet to the specified file stream.
