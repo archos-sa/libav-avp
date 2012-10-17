b7a476a8e2e8e6159abbd38bdbdde36c17c22068
Thomas Guillem <guillem@archos.com>
2012-10-17 14:50:53 +0200

    build libav directly from ndk-build

---
+++ jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/../libavcodec/Android.mk
+include $(LOCAL_PATH)/../libswscale/Android.mk
+include $(LOCAL_PATH)/../libavutil/Android.mk
+include $(LOCAL_PATH)/../libavfilter/Android.mk
+include $(LOCAL_PATH)/../libavformat/Android.mk
