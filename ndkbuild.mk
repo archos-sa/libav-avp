6e5d0726ecc38e8d0e492963f647a98dbf985aad
Thomas Guillem <guillem@archos.com>
2012-12-12 11:37:39 +0100

    don't build/install not needed libs

--- ndkbuild.mk
+++ ndkbuild.mk
@@ -1,6 +1,6 @@
 include $(LIBAV_DIR)/libavcodec/Android.mk
-include $(LIBAV_DIR)/libswscale/Android.mk
+#include $(LIBAV_DIR)/libswscale/Android.mk
 include $(LIBAV_DIR)/libavutil/Android.mk
-include $(LIBAV_DIR)/libavfilter/Android.mk
+#include $(LIBAV_DIR)/libavfilter/Android.mk
 include $(LIBAV_DIR)/libavformat/Android.mk
-include $(LIBAV_DIR)/libavresample/Android.mk
+#include $(LIBAV_DIR)/libavresample/Android.mk
