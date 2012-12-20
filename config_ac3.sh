cc066ac513749ce38bdf892f2ffd4840def5362f
Thomas Guillem <guillem@archos.com>
2012-12-20 11:23:23 +0100

    add ac3 config

---
+++ config_ac3.sh
@@ -0,0 +1,4 @@
+. ./config_base.sh
+CONFIG_LIBAV="${CONFIG_LIBAV} \
+	--enable-decoder=ac3 \
+	--enable-demuxer=ac3"
---
+++ ndk/ac3/jni/Android.mk
@@ -0,0 +1,6 @@
+LIBAV_CONFIG_TYPE := ac3
+
+LOCAL_PATH := $(call my-dir)
+LIBAV_DIR := $(LOCAL_PATH)/../../..
+
+include $(LIBAV_DIR)/ndkbuild.mk
