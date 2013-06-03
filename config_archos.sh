1e7809d9215c99c159faa0cbde571d34e58231ac
Thomas Guillem <guillem@archos.com>
2013-06-03 11:03:17 +0200

    wma only for archos

--- config_archos.sh
+++ config_archos.sh
@@ -1,4 +1,7 @@
 . ./config_base.sh
 CONFIG_LIBAV="${CONFIG_LIBAV} \
+	--enable-decoder=wmav2 \
+	--enable-decoder=wmapro \
+	--enable-decoder=wmavoice \
 	--enable-decoder=ac3 \
 	--enable-decoder=dca"
