ad4c24ae1a64a9f2a196e06d5718fea64f664ea2
Ben Avison <bavison@riscosopen.org>
2013-08-05 13:12:49 +0100

    mpegts: Remove one 64-bit integer modulus operation per packet
    The common case of the pointer having increased by one packet (which results
    in no change to the modulus) can be detected with a 64-bit subtraction,
    which is far cheaper than a division on many platforms.
    
               Before          After
               Mean   StdDev   Mean   StdDev  Change
    Divisions   248.3  8.8      51.5   7.4    +381.7%
    Overall    2773.2 25.6     2372.5 43.1     +16.9%
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -28,6 +28,7 @@
 #include "libavutil/opt.h"
 #include "libavcodec/bytestream.h"
 #include "libavcodec/get_bits.h"
+#include "libavcodec/mathops.h"
 #include "avformat.h"
 #include "mpegts.h"
 #include "internal.h"
@@ -98,6 +99,8 @@ struct MpegTSContext {
     int raw_packet_size;
 
     int pos47;
+    /** position corresponding to pos47, or 0 if pos47 invalid */
+    int64_t pos;
 
     /** if true, all pids are analyzed to find streams       */
     int auto_guess;
@@ -1775,7 +1778,7 @@ av_log(s, AV_LOG_ERROR, "GOT CODECS!\n");
         return 0;
 
     pos = avio_tell(ts->stream->pb);
-    ts->pos47= pos % ts->raw_packet_size;
+    MOD_UNLIKELY(ts->pos47, pos, ts->raw_packet_size, ts->pos);
 
     if (tss->type == MPEGTS_SECTION) {
         if (is_start) {
